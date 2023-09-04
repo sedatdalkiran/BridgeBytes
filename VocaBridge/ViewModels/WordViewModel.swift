48fd82090c283895411ec4aa809dd0b8daec8259
Sedat <sedatdlkrn46@gmail.com>
2023-09-05 00:58:32 +0300

    Fixed checking box bug for Progress Bar

--- VocaBridge/ViewModels/WordViewModel.swift
+++ VocaBridge/ViewModels/WordViewModel.swift
@@ -19,27 +19,20 @@ class WordViewModel: ObservableObject {
     @Published var allLevels: [WordLevel] = []
     @Published var progress: [Int: Bool] {
         didSet {
-            // Storing progress changes to UserDefaults
             saveProgressToUserDefaults()
         }
     }
     @Published var allLevelGroups: [WordLevelGroup] = []
 
-    
-    // MARK: - Computed Properties
-    var completedLevels: Double {
-        return Double(progress.filter { $1 }.count)
-    }
-    
     // MARK: - Initializers
     init() {
-            self.words = []
-            self.allLevels = []
-            self.progress = [:]
-
-            fetchWords()
-            loadProgress()
-        }
+        self.words = []
+        self.allLevels = []
+        self.progress = [:]
+        self.allLevelGroups = []
+        fetchWords()
+        loadProgress()
+    }
     
     // MARK: - Methods
     func fetchWords() {
@@ -69,29 +62,20 @@ class WordViewModel: ObservableObject {
             allLevels.append(WordLevel(id: level + 1, words: levelWords))
         }
     }
+
     func groupLevelsIntoGroups() {
-        let wordsPerLevel = 10
-        let totalLevels = words.count / wordsPerLevel
         let levelsPerGroup = 10
         
-        var allTempLevels: [WordLevel] = []
-        
-        for level in 0..<totalLevels {
-            let startIndex = level * wordsPerLevel
-            let endIndex = startIndex + wordsPerLevel
-            let levelWords = Array(words[startIndex..<endIndex])
-            allTempLevels.append(WordLevel(id: level + 1, words: levelWords))
-        }
-        
-        for group in stride(from: 0, to: allTempLevels.count, by: levelsPerGroup) {
-            let groupLevels = Array(allTempLevels[group..<min(group + levelsPerGroup, allTempLevels.count)])
+        for group in stride(from: 0, to: allLevels.count, by: levelsPerGroup) {
+            let groupLevels = Array(allLevels[group..<min(group + levelsPerGroup, allLevels.count)])
             allLevelGroups.append(WordLevelGroup(id: group/levelsPerGroup + 1, levels: groupLevels))
         }
     }
+
     func completedLevelsForGroup(_ groupId: Int) -> Double {
-        let startIndex = (groupId - 1) * 10
-        let endIndex = startIndex + 10
-        return Double(progress.filter { $0.key >= startIndex && $0.key < endIndex && $0.value }.count)
+        let startIndex = (groupId - 1) * 10 + 1
+        let endIndex = startIndex + 9
+        return Double(progress.filter { $0.key >= startIndex && $0.key <= endIndex && $0.value }.count)
     }
     
     func toggleProgress(for level: Int) {
